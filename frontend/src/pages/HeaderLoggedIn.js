407b093e4c5e406091c92e22db7b1e9527955bc1
callistadem <callista.dembegiotis@gmail.com>
2024-03-27 21:03:04 +0100

    added "dashboard"

--- frontend/src/pages/HeaderLoggedIn.js
+++ frontend/src/pages/HeaderLoggedIn.js
@@ -6,8 +6,9 @@ function HeaderLoggedIn() {
     return (
         <header className="row">
             <div className="col-md-2"><img src="/imgs/gomibologo.png" className="logoHeader" alt="logo"/></div>
-            <div className="col-md-4"></div>
+            <div className="col-md-2"></div>
             <div className="col-md-2 header-btn"><Link to="/report">Report</Link></div>
+            <div className="col-md-2 header-btn"><Link to="/dashboard">Dashboard</Link></div>
             <div className="col-md-2 header-btn"><Link to="/questions">Questions</Link></div>
             <div className="col-md-2 header-btn">Logout</div>
         </header>
