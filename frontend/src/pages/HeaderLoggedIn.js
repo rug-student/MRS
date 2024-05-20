01a10001963089faead32b8ac6265506435d1bd0
callistadem <callista.dembegiotis@gmail.com>
2024-05-21 00:46:35 +0200

    Made logo in header link to homepage

--- frontend/src/pages/HeaderLoggedIn.js
+++ frontend/src/pages/HeaderLoggedIn.js
@@ -5,13 +5,9 @@ import { Link } from 'react-router-dom';
 function HeaderLoggedIn() {
     return (
         <header className="row">
-            <div className="col-md-2">
-                <Link to="/home"> {/* Aggiungi il Link qui */}
-                    <img src="/imgs/gomibologo.png" className="logoHeader" alt="logo"/>
-                </Link>
-            </div>
+            <div className="col-md-2"><Link to="/"><img src="/imgs/gomibologo.png" className="logoHeader" alt="logo"/></Link> </div>
             <div className="col-md-2"></div>
-            <div className="col-md-2 header-btn"><Link to="/">Report</Link></div>
+            <div className="col-md-2 header-btn"><Link to="/report">Report</Link></div>
             <div className="col-md-2 header-btn"><Link to="/dashboard">Dashboard</Link></div>
             <div className="col-md-2 header-btn"><Link to="/questions">Questions</Link></div>
             <div className="col-md-2 header-btn"><Link to="/">Logout</Link></div>
