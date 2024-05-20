2be68fc29951acfcce79f7a65857dcc93ee9cb68
Jose Luis Ceballos <m.mura.3@student.rug.nl>
2024-05-20 17:47:15 +0200

    EDIT, dashboard changes

--- frontend/src/pages/Dashboard.jsx
+++ frontend/src/pages/Dashboard.jsx
@@ -1,91 +1,50 @@
 import React, { useEffect, useState } from "react";
-import classes from "./Dashboard.module.css";
-import { FiEdit } from "react-icons/fi";
-import Header from "./HeaderLoggedIn";
+import moment from 'moment';
+import { Container } from 'react-bootstrap';
 
 const Dashboard = () => {
 
-  const [reports, setReports] = useState([]);
+  const [reportsData, setReportsData] = useState([{ id: 1, description: "Description", priority: -1, created_at: "2024-05-15T19:30:06.000000Z", updated_at: "2024-05-15T19:30:06.000000Z", status: 0, submitter_email: "test@test.com", maintainer_id: null, response: [] }])
 
-  // loads questions into form upon page load
   useEffect(() => {
-    fetchReports();
-  }, []);
-
-  // Gets all the reports from the database
-  const fetchReports = () => {
-    fetch(`http://localhost:8000/api/reports?status=&priority=`, {
-      method: 'GET',
-      headers: {
-        'Accept' : 'application/json'
+    const getData = async () => {
+      try {
+        const response = await fetch(`http://localhost/api/reports`);
+        const data = await response.json();
+        // setReportsData(data)
+      } catch (error) {
+        console.log(error);
       }
-    })
-      .then(response => {
-        if (response.ok) {
-          return response.json();
-        } else {
-          throw new Error('Failed to fetch reports');
-        }
-      })
-      .then(reports => {
-        setReports(reports);
-      })
-      .catch(error => {
-        console.error('Error occurred while fetching reports:', error);
-      });
-  };
-  
-
+    }
+    getData()
+  }, [])
+  // { "id": 1, "description": "Description", "priority": -1, "created_at": "2024-05-15T19:30:06.000000Z", "updated_at": "2024-05-15T19:30:06.000000Z", "status": 0, "submitter_email": "test@test.com", "maintainer_id": null, "response": [] },
   return (
-    <div className={`main_wrapper_container ${classes.mainWrapper}`}>
-        <Header/>
-      <div className={`inner_wrapper_container ${classes.tableHeader}`}>
-        <div>
-          <h2>ID</h2>
-        </div>
-        <div>
-          <h2>Description</h2>
-        </div>
-        <div>
-          <h2>DateTime</h2>
-        </div>
-        <div>
-          <h2>Status</h2>
-        </div>
-        <div>
-          <h2>Email</h2>
-        </div>
-        <div>
-          <h2>Priority</h2>
-        </div>
-      </div>
-      <div className={`inner_wrapper_container ${classes.rowsWrapper}`}>
-        {reports.map((element) => (
-          <div className={classes.rowWrapper} key={element.id}>
-            <div>
-              <p>{element.id}</p>
-            </div>
-            <div>
-              <p>{element.description}</p>
-            </div>
-            <div>
-              <p>{element.created_at}</p>
-            </div>
-            <div>
-              <p>{element.status}</p>
-            </div>
-            <div>
-              <p>{element.submitter_email}</p>
-            </div>
-            <div>
-              <p>{element.priority}</p>
-              <FiEdit />
-            </div>
-          </div>
+    <Container>
+      <table class="table table-primary">
+        <h3>Primary Variant</h3>
+        <tr>
+          <th>ID</th>
+          <th>EMAIL</th>
+          <th>DESCRIPTION</th>
+          <th>PRIORITY</th>
+          <th>STATUS</th>
+          <th>CREATED AT</th>
+
+        </tr>
+        {reportsData.map(element => (
+          <tr key={element.id}>
+            <td>{element.id}</td>
+            <td>{element.submitter_email}</td>
+            <td>{element.description}</td>
+            <td>{element.priority}</td>
+            <td>{element.status}</td>
+            <td>{moment(element.created_at).fromNow()}</td>
+          </tr>
         ))}
-      </div>
-    </div>
-  );
+      </table>
+    </Container>
+  )
 };
 
 export default Dashboard;
