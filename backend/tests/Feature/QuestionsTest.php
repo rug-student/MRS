a246cf1e2b3140a66a5d604e2326019cf9dbb29c
Leo <l.a.m.willmann@student.rug.nl>
2024-05-26 17:02:44 +0200

    Question endpoint tests with id's FT-QE1 to FT-QE10

--- backend/tests/Feature/QuestionsTest.php
+++ backend/tests/Feature/QuestionsTest.php
@@ -12,86 +12,105 @@ class QuestionsTest extends TestCase
 
     use RefreshDatabase;  // Refresh the testing database after each test.
 
+    /**
+     * Test that checks if creating a open question adds it to the database.
+     */
+    public function test_create_question_with_invalid_request(): void {
+        $question_payload = [
+            "question_description"=> null,
+            "is_open"=> true,
+            "answers"=> []
+        ];
+        $request = $this->json('post', '/api/questions', $question_payload);
+        $request->assertStatus(422);
+        $this->assertDatabaseCount('questions', 0);
+
+        $question_payload = [
+            "question_description"=> "not null",
+            "is_open"=> null,
+            "answers"=> []
+        ];
+        $request = $this->json('post', '/api/questions', $question_payload);
+        $request->assertStatus(422);
+        $this->assertDatabaseCount('questions', 0);
+
+        $question_payload = [
+            "question_description"=> "not null",
+            "is_open"=> "test",
+            "answers"=> []
+        ];
+        $request = $this->json('post', '/api/questions', $question_payload);
+        $request->assertStatus(422);
+        $this->assertDatabaseCount('questions', 0);
+    }
 
     /**
-     * Test that checks if creating a mc question works as expected.
+     * Test that checks if creating a mc question enters that question in the database
+     * in addition to answer entries for each mc option where the question_id is the foreign key.
      */
     public function test_create_mc_question(): void {
 
-        $example_mc_question_body = [
-                'id' => 1,
-                'question_description'=>"Is this a multiple choice test question?",
-                'is_open'=>false,
-                'is_active'=>true,
-            ];
-
-        $mc_question_payload = [
-            "question_description"=> "Is this a multiple choice test question?",
-            "is_open"=> false,
-            "answers"=> [
-            "yes",
-            "no"
-            ]
-        ];
-
-        $request = $this->json('post', '/api/questions', $mc_question_payload);
-        $request->assertStatus(200);
-        $this->assertDatabaseHas('questions', $example_mc_question_body);
-        $this->assertDatabaseCount('questions', 1);
-    }
-
-    /**
-     * Basic test to check status and structure of get questions request.
-     */
-    public function test_get_questions(): void {
-        $response = $this->get('/api/questions');
-        $response->assertStatus(200);
-        $response->assertJsonStructure([]);
-    }
-
-    /**
-     * Test to check if retrieving a single question works as expected.
-     */
-    public function test_get_question(): void {
-        $response = $this->get('/api/questions/1');
-        $response->assertStatus(404);
-
-        $question = Question::create([
+        $question_body = [
             'question_description'=>"Is this a multiple choice test question?",
             'is_open'=>false,
             'is_active'=>true,
-        ]);
-
-        $response = $this->get("/api/questions/".(string)$question->id);
-        $response->assertStatus(200);
-    }
-
-    /**
-     * Test to check if updating a questions status works as expected.
-     */
-    public function test_patch_question(): void {
-
-        $payload = [
-            'is_active' => false
+        ];
+        $answer_option1 = "yes";
+        $answer_option2 = "no";
+        $question_payload = [
+            "question_description"=> "Is this a multiple choice test question?",
+            "is_open"=> false,
+            "answers"=> [
+                $answer_option1,
+                $answer_option2
+            ]
         ];
 
-        $question = Question::create([
-            'question_description'=>"Is this an update test question?",
-            'is_open'=>false,
-            'is_active'=>true,
-        ]);
-        $this->assertDatabaseCount('questions', 1);
-
-
-        $response = $this->patch("api/questions/".(string)$question->id, $payload);
+        $response = $this->json('post', '/api/questions', $question_payload);
+        // Question entry asserts
         $response->assertStatus(200);
-
+        $this->assertDatabaseHas('questions', $question_body);
         $this->assertDatabaseCount('questions', 1);
-        $this->assertDatabaseHas('questions', $payload);
+
+        // Answer entry asserts with question_id as foreign key
+        $answer1_body = [
+            "answer"=>$answer_option1,
+            "question_id"=>json_decode($response->content())[1]->id
+        ];
+        $answer2_body = [
+            "answer"=>$answer_option2,
+            "question_id"=>json_decode($response->content())[1]->id
+        ];
+        $this->assertDatabaseHas('answers', $answer1_body);
+        $this->assertDatabaseHas('answers', $answer2_body);
+        $this->assertDatabaseCount('answers', 2);
     }
 
     /**
-     * Test that checks if creating a open question works as expected.
+     * Test if creating a mc question with invalid answer option returns and error and does not store any question.
+     */
+    public function test_create_mc_question_with_null_answer(): void {
+        $question_payload = [
+            "question_description"=> "Is this a multiple choice test question?",
+            "is_open"=> false,
+            "answers"=> [
+                "yes",
+                "no",
+                "test",
+                null,
+                "Test2"
+            ]
+        ];
+
+        $response = $this->json('post', '/api/questions', $question_payload);
+        // Question entry asserts
+        $response->assertStatus(400);
+        $this->assertDatabaseCount('questions', 0);
+        $this->assertDatabaseCount('answers', 0);
+    }
+
+    /**
+     * Test that checks if creating a question with no mc answers adds it to the database.
      */
     public function test_create_open_question(): void {
 
@@ -104,14 +123,130 @@ class QuestionsTest extends TestCase
         $open_question_payload = [
             "question_description"=> "Is this a multiple choice test question?",
             "is_open"=> true,
-            "answers"=> [
-            ""
-            ]
+            "answers"=> []
         ];
 
         $request = $this->json('post', '/api/questions', $open_question_payload);
         $request->assertStatus(200);
         $this->assertDatabaseHas('questions', $example_open_question_body);
         $this->assertDatabaseCount('questions', 1);
+        $this->assertDatabaseCount('answers', 0);
     }
+
+    /**
+     * Basic test to check status and structure of get questions request.
+     */
+    public function test_get_questions_on_empty_database(): void {
+        $response = $this->get('/api/questions');
+        $response->assertStatus(200);
+        $response->assertContent("[]");
+    }
+
+    /**
+     * Test get questions request response on populated database.
+     */
+    public function test_get_questions_on_populated_database(): void {
+        $question1 = Question::create([
+            'question_description'=>"This is a test question",
+            'is_open'=>true,
+            'is_active'=>true,
+        ]);
+
+        $question2 = Question::create([
+            'question_description'=>"This is a second question",
+            'is_open'=>false,
+            'is_active'=>false,
+        ]);
+
+        $response = $this->get('/api/questions');
+        $response->assertStatus(200);
+
+        // Assert question properties
+        $response->assertSee((string)$question1->id);
+        $response->assertSee($question1->question_description);
+        $response->assertSee($question1->is_open);
+        $response->assertSee($question1->is_active);
+
+        $response->assertSee((string)$question2->id);
+        $response->assertSee($question2->question_description);
+        $response->assertSee($question2->is_open);
+        $response->assertSee($question2->is_active);
+    }
+
+    /**
+     * Test retrieving on nonexisting question id.
+     */
+    public function test_get_question_invalid_question_id(): void {
+        $response = $this->get('/api/questions/1');
+        $response->assertStatus(404);
+    }
+
+    /**
+     * Test to check if retrieving a single question retreives all data.
+     */
+    public function test_get_question_valid_question_id(): void {
+
+        $question = Question::create([
+            'question_description'=>"This is a test question",
+            'is_open'=>true,
+            'is_active'=>true,
+        ]);
+
+        $response = $this->get("/api/questions/".(string)$question->id);
+        $response->assertStatus(200);
+
+        // Assert question properties
+        $response->assertSee((string)$question->id);
+        $response->assertSee($question->question_description);
+        $response->assertSee($question->is_open);
+        $response->assertSee($question->is_active);
+    }
+
+    /**
+     * Test if updating a questions with invalid request give error.
+     */
+    public function test_patch_question_invalid_request(): void {
+
+        $patch_payload = [
+            "is_active"=>"false" // not boolean value
+        ];
+
+        $question_payload = [
+            'question_description'=>"Is this an update test question?",
+            'is_open'=>false,
+            'is_active'=>true,
+        ];
+        $question = Question::create($question_payload);
+
+        $this->assertDatabaseCount('questions', 1);
+        $response = $this->json('PATCH', "api/questions/".(string)$question->id, $patch_payload); // need to use propper json request here.
+        $response->assertStatus(422);
+        $this->assertDatabaseHas('questions', $question_payload); // check for unchanged question.
+        $this->assertDatabaseCount('questions', 1);
+    }
+
+    /**
+     * Test to check if updating a questions status works as expected.
+     */
+    public function test_patch_question_valid_request(): void {
+
+        $payload = [
+            'is_active' => false
+        ];
+
+        $question = Question::create([
+            'question_description'=>"Is this an update test question?",
+            'is_open'=>false,
+            'is_active'=>true,
+        ]);
+        $this->assertDatabaseCount('questions', 1);
+
+        $response = $this->json('PATCH', "api/questions/".(string)$question->id, $payload);
+        $response->assertStatus(200);
+
+        $this->assertDatabaseCount('questions', 1);
+        $this->assertDatabaseHas('questions', $payload);
+    }
+
+
 }
