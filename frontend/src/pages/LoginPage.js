deb2bde70a089fab73018d71921cc39f0ccce7b9
callistadem <callista.dembegiotis@gmail.com>
2024-05-26 23:34:33 +0200

    made login page responsive

--- frontend/src/pages/LoginPage.js
+++ frontend/src/pages/LoginPage.js
@@ -3,12 +3,11 @@ import './LoginPage.css';
 import Header from '../components/Header';
 import useAuthContext from '../context/AuthContext';
 
-function App() {
+function LoginPage() {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
   const [message, setMessage] = useState('');
   const { login, errors } = useAuthContext();
-
 
   const handleSubmit = async (e) => {
     e.preventDefault();
@@ -17,36 +16,38 @@ function App() {
   };
 
   return (
-    <div class="p">
+    <div class="page">
     <Header />
-      <div id="logform1">
-        <h1>Login</h1>
-          <form onSubmit={handleSubmit}>
+      <div class="login-page">
+        <div id="logform1">
+          <h1>Login</h1>
+            <form onSubmit={handleSubmit}>
+              <div>
+                <label class="labuno">Email:</label>
+                  <input
+                    type="text"
+                    value={email}
+                    onChange={(e) => setEmail(e.target.value)}
+                    id ="inuno"
+                  />
+            </div>
             <div>
-              <label class="labuno">Email:</label>
-                <input
-                  type="text"
-                  value={email}
-                  onChange={(e) => setEmail(e.target.value)}
-                  id ="inuno"
-                />
-          </div>
-          <div>
-            <label class="labuno">Password:</label>
-            <input
-              type="password"
-              value={password}
-              onChange={(e) => setPassword(e.target.value)}
-              id="indue"
-            />
-          </div>
-        <input type="hidden" name="_token" value="{{ csrf_token() }}" />
-      <button id="subutton" type="submit">Login</button>
-    </form>
-    <p>{message}</p>
+              <label class="labuno">Password:</label>
+              <input
+                type="password"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+                id="indue"
+              />
+            </div>
+          <input type="hidden" name="_token" value="{{ csrf_token() }}" />
+        <button id="subutton" type="submit">Login</button>
+      </form>
+      <p>{message}</p>
+    </div>
   </div>
 </div>
   );
 }
 
-export default App;
+export default LoginPage;
