63fac911d6f4616ec968ae47d9416aea6270fa4f
Leo <l.a.m.willmann@student.rug.nl>
2024-05-26 14:26:42 +0200

    Post answer endpoint can now only create answers with the 'answer' field. Cannot add a foreign question_id field through this endpoint

--- backend/app/Http/Controllers/AnswersController.php
+++ backend/app/Http/Controllers/AnswersController.php
@@ -12,23 +12,24 @@ class AnswersController extends Controller
      * Create a new Answer.
      */
     public function createAnswer(Request $request) {
-        $answer = new Answer;
 
         $request->validate([
             "answer" => "required"
         ]);
 
-        if ($request->question_id == null) {
-            $answer->answer = $request->answer;
-            $answer->save();
-        } else {
+        $answer = new Answer;
+        $answer->answer = $request->answer;
+
+        /* Uncomment to allow for post answer endpoint add question_id as foreign id in the answer entry to the database.
+        if ($request->question_id != null) {
             if(!Question::where("id", $request->question_id)->exists()) {
                 return response()->json("ERROR: passed non-existing question", 400);
             }
-            $answer->answer = $request->answer;
             $answer->question_id = $request->question_id;
-            $answer->save();
         }
+        */
+
+        $answer->save();
         return response()->json(["succesfully created answer", $answer], 200);
     }
 }
