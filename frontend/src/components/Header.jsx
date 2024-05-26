e5f28a7eb042d5f3245971d4b470eb67632247d1
callistadem <callista.dembegiotis@gmail.com>
2024-05-26 23:06:33 +0200

    Reponsive header

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,33 +1,52 @@
-import React, { useEffect, useState } from 'react';
+import React, { useState, useEffect} from 'react';
 import './Header.css';
 import { Link } from 'react-router-dom';
 import useAuthContext from '../context/AuthContext';
+import MenuIcon from '@mui/icons-material/Menu'; 
 
 function Header() {
+    const { logout, user } = useAuthContext();
+    const [showMenu, setShowMenu] = useState(false);
 
-	const { logout, user } = useAuthContext();
+    const toggleMenu = () => {
+        setShowMenu(!showMenu);
+    };
 
-	return (
-		<header className="row">
-			<div className="col-md-2"><Link to="/"><img src="/imgs/gomibologo.png" className="logoHeader" alt="logo" /></Link> </div>
-			{user ? ( <>
-					<div className="col-md-2"></div>
-					<div className="col-md-2 header-btn"><Link to="/report">Report</Link></div>
-					<div className="col-md-2 header-btn"><Link to="/dashboard">Dashboard</Link></div>
-					<div className="col-md-2 header-btn"><Link to="/questions">Questions</Link></div>
-					<div className="col-md-2 header-btn" onClick={logout}><Link to="/">Logout</Link></div>
-				</>
-			) : (
- 				<>
-				<div className="col-md-5"></div>
-				<div className="col-md-2 header-btn"><Link to="/report">Report</Link></div>
-				<div className="col-md-2 header-btn"><Link to="/login">Login</Link></div>
-				<div className="col-md-1"></div>
-				</>
-			)}
-			
-		</header>
-	);
+	useEffect(() => {
+        const headerElement = document.querySelector('header');
+        if (showMenu) {
+            headerElement.classList.add('expand');
+        } else {
+            headerElement.classList.remove('expand');
+        }
+    }, [showMenu]);
+
+    return (
+        <header className="align-items-center">
+            <Link className='logo' to="/">
+                <img src="/imgs/gomibologo.png" className="logoHeader" alt="logo" />
+            </Link>
+            
+            <div className={`menu-items ${showMenu ? 'show' : 'noShow'}`}>
+                {user ? (
+                    <>
+                        <div className="header-btn"><Link to="/report">Report</Link></div>
+                        <div className="header-btn"><Link to="/dashboard">Dashboard</Link></div>
+                        <div className="header-btn"><Link to="/questions">Questions</Link></div>
+                        <div className="header-btn" onClick={logout}><Link to="/">Logout</Link></div>
+                    </>
+                ) : (
+                    <>
+                        <div className="header-btn"><Link to="/report">Report</Link></div>
+                        <div className="header-btn"><Link to="/login">Login</Link></div>
+                    </>
+                )}
+            </div>
+            <div className="hamburger" onClick={toggleMenu}>
+                <MenuIcon />
+            </div>
+        </header>
+    );
 }
 
 export default Header;
