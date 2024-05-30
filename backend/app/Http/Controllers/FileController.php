a0ab986d46bb2bf9ec2c535289bb3516ab77ee52
callistadem <callista.dembegiotis@gmail.com>
2024-05-30 21:09:18 +0200

    backend fileUpload POST request working (Postman test)

---
+++ backend/app/Http/Controllers/FileController.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\File;
+use Illuminate\Http\JsonResponse;
+use Illuminate\Http\Request;
+use Illuminate\Support\Str;
+
+class FileController extends Controller
+{
+    // Receives a file upload
+    public function upload(Request $request): JsonResponse
+    {
+        $file = $request->file('file');
+        $fileName = Str::random(20) . '.' . $file->getClientOriginalExtension();
+        $file->storeAs('uploads', $fileName);
+
+        File::create([
+            'original_name' => $file->getClientOriginalName(),
+            'generated_name' => $fileName,
+        ]);
+
+        return response()->json(['message' => 'File uploaded successfully']);
+    }
+
+    // Returns a list of previously uploaded files
+    public function index(): JsonResponse
+    {
+        $files = File::all();
+        return response()->json($files);
+    }
+
+    // Sends the requested file as a download, if found
+    public function download(File $file)
+    {
+        $filePath = storage_path("app/uploads/{$file->generated_name}");
+
+        if (file_exists($filePath)) {
+            return response()->download($filePath, $file->original_name);
+        } else {
+            abort(404, 'File not found');
+        }
+    }
+
+}
--- backend/app/Models/File.php
+++ backend/app/Models/File.php
@@ -4,27 +4,13 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
-/**
- * Model representing a File upload.
- *
- * Many files belong to 1 Report.
-*/
+
 class File extends Model
 {
     use HasFactory;
 
-    public function report() {
-        return $this->belongsTo(Report::class);
-    }
-
-    protected $fillable= [
-        "file_path",
+    protected $fillable = [
+        'original_name',
+        'generated_name',
     ];
-    protected $hidden= [
-        "report_id"
-    ];
-
-    
-
-    //created_at & updated_at are automatically updated by the ORM
 }
