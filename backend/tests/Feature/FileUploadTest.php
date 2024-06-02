bddcb0c149105ee69ad2f3071a8e5c5b2f7c5588
Leo <l.a.m.willmann@student.rug.nl>
2024-06-02 12:23:15 +0200

    Added file endpoint tests with id FT-FE 1-7 testing file upload and download with faked file

---
+++ backend/tests/Feature/FileUploadTest.php
@@ -0,0 +1,127 @@
+<?php
+
+namespace Tests\Feature;
+
+use App\Models\Answer;
+use App\Models\Question;
+use App\Models\Report;
+use App\Models\Response;
+use App\Models\File;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Laravel\Sanctum\Sanctum;
+use Illuminate\Http\UploadedFile;
+use App\Models\User;
+use Tests\TestCase;
+
+class FileUploadTest extends TestCase
+{
+
+    use RefreshDatabase;
+
+
+    /**
+     * FT-FE1
+     * Test uploading endpoint with no file.
+     */
+    public function test_upload_with_no_file(): void {
+
+        $response = $this->json('post', '/api/files/upload');
+        $response->assertUnprocessable();
+    }
+
+    /**
+     * FT-FE2
+     * Test uploading endpoint with csv file expecting 422 response as only images are allowed.
+     */
+    public function test_upload_with_csv_file(): void {
+
+        $file = UploadedFile::fake()->createWithContent('customer.csv', 'id;name;group\n9999;Customer1;');
+        $response = $this->json('post', '/api/files/upload', [
+            'file' => $file
+        ]);
+        $response->assertUnprocessable();
+    }
+
+
+    /**
+     * FT-FE3
+     * Test uploading endpoint with image but no report id.
+     */
+    public function test_upload_with_image_no_report_id(): void {
+
+        $file = UploadedFile::fake()->image("testImage.png");
+        $response = $this->json('post', '/api/files/upload', [
+            'file' => $file
+        ]);
+        $response->assertBadRequest();
+        $response->assertSee("ERROR: passed non-existing report id");
+    }
+
+    /**
+     * FT-FE4
+     * Test uploading endpoint with image and invalid report id.
+     */
+    public function test_upload_with_image_and_invalid_report_id(): void {
+
+        $file = UploadedFile::fake()->image("testImage.png");
+        $response = $this->json('post', '/api/files/upload', [
+            'file' => $file,
+            'report_id' => 1,
+        ]);
+        $response->assertBadRequest();
+        $response->assertSee("ERROR: passed non-existing report id");
+    }
+
+    /**
+     * FT-FE5
+     * Test uploading endpoint with image and invalid report id.
+     */
+    public function test_upload_with_image_and_valid_report_id(): void {
+        $report = Report::factory()->create();
+
+        $file = UploadedFile::fake()->image("testImage.png");
+        $response = $this->json('post', '/api/files/upload', [
+            'file' => $file,
+            'report_id' => $report->id,
+        ]);
+        $response->assertOk();
+    }
+
+    /**
+     * FT-FE6
+     * Test download endpoint with invalid file id.
+     */
+    public function test_download_invalid_file_name(): void {
+        $report = Report::factory()->create();
+
+        $file = UploadedFile::fake()->image("testImage.png");
+        $response = $this->json('post', '/api/files/upload', [
+            'file' => $file,
+            'report_id' => $report->id,
+        ]);
+        $response->assertOk();
+
+        $fileID = json_decode($response->getContent())->file->id;
+        $response = $this->json('get', '/api/files/'.($fileID+1).'/download');
+        $response->assertNotFound();
+    }
+
+    /**
+     * FT-FE7
+     * Test download endpoint with valid file id.
+     */
+    public function test_download_valid_file_name(): void {
+        $report = Report::factory()->create();
+
+        $file = UploadedFile::fake()->image("testImage.png");
+        $response = $this->json('post', '/api/files/upload', [
+            'file' => $file,
+            'report_id' => $report->id,
+        ]);
+        $response->assertOk();
+
+        $fileID = json_decode($response->getContent())->file->id;
+        $response = $this->json('get', '/api/files/'.$fileID.'/download');
+        $response->assertOk();
+    }
+}
--- backend/tests/Feature/ReportsTest.php
+++ backend/tests/Feature/ReportsTest.php
@@ -11,6 +11,7 @@ use Illuminate\Foundation\Testing\RefreshDatabase;
 use Laravel\Sanctum\Sanctum;
 use App\Models\User;
 use Tests\TestCase;
+use Illuminate\HTtp\UploadedFile;
 
 class ReportsTest extends TestCase
 {
@@ -202,119 +203,119 @@ class ReportsTest extends TestCase
         ];
         $this->assertDatabaseHas('responses', $response_body);
     }
-    /**
-     * FT-RE10
-     * Test POST /reports endpoint with files
-     */
-    public function test_create_report_with_files(): void {
-        $file_path1 = "file/path1";
-        $file_path2 = "file/path2";
+    // /**
+    //  * FT-RE10
+    //  * Test POST /reports endpoint with files
+    //  */
+    // public function test_create_report_with_files(): void {
+    //     $file_path1 = "file/path1";
+    //     $file_path2 = "file/path2";
 
-        $report_payload = [
-            'description'=>"This is a test report",
-            'submitter_email'=>"test@testing.nl",
-            'notify_submitter'=>false,
-            'files' => [
-                'a'=> [
-                    "file_path"=> $file_path1
-                ],
-                'b'=> [
-                    "file_path"=> $file_path2
-                ]
-            ]
-        ];
+    //     $report_payload = [
+    //         'description'=>"This is a test report",
+    //         'submitter_email'=>"test@testing.nl",
+    //         'notify_submitter'=>false,
+    //         'files' => [
+    //             'a'=> [
+    //                 "file_path"=> $file_path1
+    //             ],
+    //             'b'=> [
+    //                 "file_path"=> $file_path2
+    //             ]
+    //         ]
+    //     ];
 
-        $request = $this->json('post', '/api/reports', $report_payload);
-        $request->assertStatus(200);
-        $this->assertDatabaseCount('reports', 1);
-        $this->assertDatabaseCount('files', 2);
+    //     $request = $this->json('post', '/api/reports', $report_payload);
+    //     $request->assertStatus(200);
+    //     $this->assertDatabaseCount('reports', 1);
+    //     $this->assertDatabaseCount('files', 2);
 
-        $file_body = [
-            "file_path"=> $file_path1,
-            "report_id"=>json_decode($request->getContent())[1]->id
-        ];
-        $this->assertDatabaseHas('files', $file_body);
+    //     $file_body = [
+    //         "file_path"=> $file_path1,
+    //         "report_id"=>json_decode($request->getContent())[1]->id
+    //     ];
+    //     $this->assertDatabaseHas('files', $file_body);
 
-        $file_body = [
-            "file_path"=> $file_path2,
-            "report_id"=>json_decode($request->getContent())[1]->id
-        ];
-        $this->assertDatabaseHas('files', $file_body);
-    }
+    //     $file_body = [
+    //         "file_path"=> $file_path2,
+    //         "report_id"=>json_decode($request->getContent())[1]->id
+    //     ];
+    //     $this->assertDatabaseHas('files', $file_body);
+    // }
 
-    /**
-     * FT-RE11
-     * Test POST /reports endpoint with report responses and files
-     */
-    public function test_create_report_with_responses_and_files(): void {
+    // /**
+    //  * FT-RE11
+    //  * Test POST /reports endpoint with report responses and files
+    //  */
+    // public function test_create_report_with_responses_and_files(): void {
 
-        $question = Question::create([
-            'question_description'=>"This is a question",
-            'is_open'=>false,
-            'is_active'=>false,
-        ]);
-        $answer = Answer::create([
-            'answer'=>"this is an answer",
-            'question_id'=>$question->id
-        ]);
+    //     $question = Question::create([
+    //         'question_description'=>"This is a question",
+    //         'is_open'=>false,
+    //         'is_active'=>false,
+    //     ]);
+    //     $answer = Answer::create([
+    //         'answer'=>"this is an answer",
+    //         'question_id'=>$question->id
+    //     ]);
 
-        $file_path1 = "file/path1";
-        $file_path2 = "file/path2";
+    //     $file_path1 = "file/path1";
+    //     $file_path2 = "file/path2";
 
-        $report_payload = [
-            'description'=>"This is a test report",
-            'submitter_email'=>"test@testing.nl",
-            'notify_submitter'=>false,
-            'responses' => [
-                '1'=>[
-                    "question_id"=> $question->id,
-                    "answer_id"=> $answer->id
-                ],
-            ],
-            'files' => [
-                'a'=> [
-                    "file_path"=> $file_path1
-                ],
-                'b'=> [
-                    "file_path"=> $file_path2
-                ],
-            ]
-        ];
+    //     $report_payload = [
+    //         'description'=>"This is a test report",
+    //         'submitter_email'=>"test@testing.nl",
+    //         'notify_submitter'=>false,
+    //         'responses' => [
+    //             '1'=>[
+    //                 "question_id"=> $question->id,
+    //                 "answer_id"=> $answer->id
+    //             ],
+    //         ],
+    //         'files' => [
+    //             'a'=> [
+    //                 "file_path"=> $file_path1
+    //             ],
+    //             'b'=> [
+    //                 "file_path"=> $file_path2
+    //             ],
+    //         ]
+    //     ];
 
 
 
-        $request = $this->json('post', '/api/reports', $report_payload);
-        // $this->assertEquals(1,2, json_decode($request->getContent()));
-        $request->assertStatus(200);
-        $this->assertDatabaseCount('reports', 1);
-        $this->assertDatabaseCount('responses', 1);
-        $this->assertDatabaseCount('files', 2);
+    //     $request = $this->json('post', '/api/reports', $report_payload);
+    //     // $this->assertEquals(1,2, json_decode($request->getContent()));
+    //     $request->assertStatus(200);
+    //     $this->assertDatabaseCount('reports', 1);
+    //     $this->assertDatabaseCount('responses', 1);
+    //     $this->assertDatabaseCount('files', 2);
 
-        $response_body = [
-            "question_id"=> $question->id,
-            "answer_id"=> $answer->id,
-            "report_id"=>json_decode($request->getContent())[1]->id
-        ];
-        $this->assertDatabaseHas('responses', $response_body);
+    //     $response_body = [
+    //         "question_id"=> $question->id,
+    //         "answer_id"=> $answer->id,
+    //         "report_id"=>json_decode($request->getContent())[1]->id
+    //     ];
+    //     $this->assertDatabaseHas('responses', $response_body);
 
-        $file_body = [
-            "file_path"=> $file_path1,
-            "report_id"=>json_decode($request->getContent())[1]->id
-        ];
-        $this->assertDatabaseHas('files', $file_body);
+    //     $file_body = [
+    //         "file_path"=> $file_path1,
+    //         "report_id"=>json_decode($request->getContent())[1]->id
+    //     ];
+    //     $this->assertDatabaseHas('files', $file_body);
 
-        $file_body = [
-            "file_path"=> $file_path2,
-            "report_id"=>json_decode($request->getContent())[1]->id
-        ];
-        $this->assertDatabaseHas('files', $file_body);
-    }
+    //     $file_body = [
+    //         "file_path"=> $file_path2,
+    //         "report_id"=>json_decode($request->getContent())[1]->id
+    //     ];
+    //     $this->assertDatabaseHas('files', $file_body);
+    // }
 
     /**
      * FT-RE12
-     * Tests the retrieval of a single report with responses and files.
+     * Tests the retrieval of a single report with responses.
      */
-    public function test_get_report_with_responses_and_files(): void {
+    public function test_get_report_with_responses(): void {
         Sanctum::actingAs(User::factory()->create());
 
         $report = Report::factory()->create();
@@ -349,28 +350,17 @@ class ReportsTest extends TestCase
         $response->report_id = $report->id;
         $report->response()->save($response);
 
-        // File body and model entity
-        $file_body = [
-            "file_path"=> "some filepath",
-            "report_id"=>$report->id
-        ];
-        $file = new File();
-        $file->file_path = "some filepath";
-        $file->report_id = $report->id;
-        $report->file()->save($file);
 
         // Test database entry counts of the following tables
         $this->assertDatabaseCount('reports', 1);
         $this->assertDatabaseCount('questions', 1);
         $this->assertDatabaseCount('answers', 1);
         $this->assertDatabaseCount('responses', 1);
-        $this->assertDatabaseCount('files', 1);
 
         $response = $this->get('api/reports/'.$report->id);
         $response->assertStatus(200);
         $response->assertSee($report->toArray);
         $response->assertSee($response_body);
-        $response->assertSee($file_body);
     }
 
     /**
